b28d535bcdea0f8a2e2197014f336bab04eaa6a5
Rian De almeida Ferreira <64613463+orianferreira@users.noreply.github.com>
2022-08-08 15:42:59 -0300

    Update UserDetailsImpl.java

--- src/main/java/com/generation/blogpessoal/seguranca/UserDetailsImpl.java
+++ src/main/java/com/generation/blogpessoal/seguranca/UserDetailsImpl.java
@@ -1,6 +1,7 @@
 package com.generation.blogpessoal.seguranca;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,59 +9,60 @@ import org.springframework.security.core.userdetails.UserDetails;
 import com.generation.blogpessoal.model.Usuario;
 
 public class UserDetailsImpl implements UserDetails{
-	
-	private static final long serialVersionUID = 1L;
-	
-	private String userName;
-	private String password;
-	
-	public UserDetailsImpl(Usuario user) {
-		this.userName = user.getUsuario();
-		this.password = user.getSenha();
-	}
-	
-	public UserDetailsImpl() {}
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return authorities;
-	}
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return password;
-	}
+    private String userName;
+    private String password;
+    private List<GrantedAuthority> authorities;
 
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return userName;
-	}
+    public UserDetailsImpl(Usuario user) {
+        this.userName = user.getUsuario();
+        this.password = user.getSenha();
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
-	}
+    public UserDetailsImpl() {}
 
-	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return true;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        return authorities;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
-	}
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return password;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return true;
-	}
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return true;
+    }
 
 }
