a33f3969bd88417f8bc38404a939512c082cae6c
Rian De almeida Ferreira <64613463+orianferreira@users.noreply.github.com>
2022-08-16 12:20:48 -0300

    Update Postagem.java

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -21,7 +21,7 @@ public class Postagem {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 
 	@NotNull
 	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
